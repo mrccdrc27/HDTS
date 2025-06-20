b7236d9a3a0e3115e5d8832f3348f5ba3683d65d
seyxhel <sealtieljosephbpelagio@iskolarngbayan.pup.edu.ph>
2025-06-20 20:44:28 +0800

    Refactor code structure for improved readability and maintainability

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'group5capstone1',
+        'NAME': 'production',
         'USER': 'postgres',
         'PASSWORD': 'Oklangyan421!',
         'HOST': 'localhost',
