7561b260d86f5c5b5c4f7e90af3bd7753233d25f
mrccdric27 <cubecore27@gmail.com>
2025-11-06 18:50:39 +0800

    feat(ticket): add employee_cookie_id field to TicketSerializer and update read-only fields

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -195,12 +195,12 @@ class TicketSerializer(serializers.ModelSerializer):
                 'issue_type', 'other_issue', 'performance_start_date', 'performance_end_date',
                 'approved_by', 'cost_items', 'requested_budget', 'fiscal_year', 'department_input',
                 'dynamic_data', 'status', 'submit_date', 'update_date', 'assigned_to', 'attachments',
-        'employee', 'rejected_by'
+        'employee', 'employee_cookie_id', 'rejected_by'
         ]
         read_only_fields = [
             'id', 'ticket_number', 'submit_date', 'update_date',
             'response_time', 'resolution_time', 'time_closed', 'assigned_to',
-            'employee'
+            'employee', 'employee_cookie_id'
         ]
 
     def create(self, validated_data):
